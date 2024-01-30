0580e1905240aad325eaee4cf1bfb0a5a2c434d8
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2024-01-30 22:00:38 +0530

    Instagram & youtube downloader screens are created

---
+++ lib/screens/youtube_downloader_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class YoutubeDownloaderScreen extends StatelessWidget {
+  const YoutubeDownloaderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Youtube Downloader'),
+      ),
+    );
+  }
+}
