fd5c96e31074d363fee177516c27cbf4f67d6118
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2024-01-30 21:44:08 +0530

    chats leading images are updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:insta_gallery/constants/assets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,24 +9,33 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Insta Gallary'),
+        elevation: 1,
       ),
       body: ListView(
-        children: const [
+        children: [
           ListTile(
-            leading: CircleAvatar(
-              child: Icon(Icons.download),
+            leading: Image.asset(
+              ImageAssets.instagram,
+              height: 40,
+              width: 40,
             ),
-            title: Text('Instagram Downloader'),
-            subtitle: Text('Hello'),
-            trailing: Text('11:12 AM'),
+            title: const Text('Instagram Downloader'),
+            subtitle: const Text('Hello'),
+            trailing: const Text('11:12 AM'),
+          ),
+          const Divider(
+            indent: 70,
+            height: 1,
           ),
           ListTile(
-            leading: CircleAvatar(
-              child: Icon(Icons.movie),
+            leading: Image.asset(
+              ImageAssets.youtube,
+              height: 40,
+              width: 40,
             ),
-            title: Text('Youtube Downloader'),
-            subtitle: Text('Hello'),
-            trailing: Text('11:12 AM'),
+            title: const Text('Youtube Downloader'),
+            subtitle: const Text('Hello'),
+            trailing: const Text('11:12 AM'),
           ),
         ],
       ),
