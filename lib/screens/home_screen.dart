aa5424de0c509192d3873d3b6ee7f9591f9a1f58
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2024-01-30 22:01:36 +0530

    downloader screens navigation implemented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:insta_gallery/constants/app_routes.dart';
 import 'package:insta_gallery/constants/assets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -19,9 +21,14 @@ class HomeScreen extends StatelessWidget {
               height: 40,
               width: 40,
             ),
-            title: const Text('Instagram Downloader'),
+            title: Text(
+              'Instagram Downloader',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
             subtitle: const Text('Hello'),
             trailing: const Text('11:12 AM'),
+            onTap: () => GoRouter.of(context)
+                .pushNamed(AppRouteNames.instagramDownloader),
           ),
           const Divider(
             indent: 70,
@@ -33,9 +40,14 @@ class HomeScreen extends StatelessWidget {
               height: 40,
               width: 40,
             ),
-            title: const Text('Youtube Downloader'),
+            title: Text(
+              'Youtube Downloader',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
             subtitle: const Text('Hello'),
             trailing: const Text('11:12 AM'),
+            onTap: () =>
+                GoRouter.of(context).pushNamed(AppRouteNames.youtubeDownloader),
           ),
         ],
       ),
